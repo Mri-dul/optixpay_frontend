d1774f9067a0d77e8428a17ad32a61034bbb3d10
maloy-baroi <maloy.virat@gmail.com>
2024-09-14 21:02:16 +0600

    design added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+import Dashboard from "@/components/Dashboard/page";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title:
+    "OptixPay | Dashboard",
+  description: "This is 'OptixPay' which is an open payment gateway",
+};
 
 export default function Home() {
   return (
-      <></>
+    <>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
+    </>
   );
 }
